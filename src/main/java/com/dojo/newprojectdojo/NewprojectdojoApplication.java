d85aaea8f43b5b024fb983be96d8f3f73ec2bc21
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-09 17:55:09 -0300

    new project
    new project

---
+++ src/main/java/com/dojo/newprojectdojo/NewprojectdojoApplication.java
@@ -0,0 +1,13 @@
+package com.dojo.newprojectdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewprojectdojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewprojectdojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dojo/newprojectdojo/NewprojectdojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.newprojectdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewprojectdojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
