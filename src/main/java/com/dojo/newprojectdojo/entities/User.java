9c88bc11e193b6d3daa8025b606b5585328b6982
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-09 18:37:12 -0300

    mapeando entidade user para o banco de dados

--- src/main/java/com/dojo/newprojectdojo/entities/User.java
+++ src/main/java/com/dojo/newprojectdojo/entities/User.java
@@ -1,18 +1,25 @@
 package com.dojo.newprojectdojo.entities;
 
-import lombok.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-@Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
