3ccc17adaa1357d781a722f9510e479d8caaa363
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-09 22:04:17 -0300

    consertando rechamadas entre entidades

--- src/main/java/com/dojo/newprojectdojo/entities/User.java
+++ src/main/java/com/dojo/newprojectdojo/entities/User.java
@@ -1,5 +1,6 @@
 package com.dojo.newprojectdojo.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,6 +28,7 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
