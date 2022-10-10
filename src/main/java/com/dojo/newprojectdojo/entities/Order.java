95421481b7778edd135953012133bf3a9da3609c
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-09 22:13:26 -0300

    Ajustando horario

--- src/main/java/com/dojo/newprojectdojo/entities/Order.java
+++ src/main/java/com/dojo/newprojectdojo/entities/Order.java
@@ -1,5 +1,6 @@
 package com.dojo.newprojectdojo.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -23,6 +24,8 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
     @JsonIgnore
