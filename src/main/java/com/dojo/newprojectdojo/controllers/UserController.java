89bc4abd28862bd386b86fcc6b9f342e3b0b3d89
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-09 21:29:02 -0300

    Implementando Service e add mais um endpoint

--- src/main/java/com/dojo/newprojectdojo/controllers/UserController.java
+++ src/main/java/com/dojo/newprojectdojo/controllers/UserController.java
@@ -1,18 +1,32 @@
 package com.dojo.newprojectdojo.controllers;
 
 import com.dojo.newprojectdojo.entities.User;
+import com.dojo.newprojectdojo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L,"rodrigo teste","rodrigo@test.com","123456789","xpto");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- src/main/java/com/dojo/newprojectdojo/repositories/UserRepository.java
+++ src/main/java/com/dojo/newprojectdojo/repositories/UserRepository.java
@@ -2,7 +2,8 @@ package com.dojo.newprojectdojo.repositories;
 
 import com.dojo.newprojectdojo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-
 }
