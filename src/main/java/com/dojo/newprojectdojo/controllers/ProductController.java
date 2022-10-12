e5481ae63b2debc3defb84ff1bd03f4b3f1fd91c
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-12 14:59:33 -0300

    correcao controller de product

--- src/main/java/com/dojo/newprojectdojo/controllers/ProductController.java
+++ src/main/java/com/dojo/newprojectdojo/controllers/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/product")
+@RequestMapping(value = "/products")
 public class ProductController {
 
     @Autowired
