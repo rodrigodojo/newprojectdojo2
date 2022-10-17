ce4a1b99dff60c1b5341fb9819ac50e5588763e8
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-17 16:17:41 -0300

    criando erros com HttpCodes personalizados parte 2

---
+++ src/main/java/com/dojo/newprojectdojo/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.dojo.newprojectdojo.controllers.exceptions;
+
+import com.dojo.newprojectdojo.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError erro = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(erro);
+    }
+}
