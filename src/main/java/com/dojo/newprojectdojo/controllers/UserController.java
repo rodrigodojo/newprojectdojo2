40afe099b31d40e515ec73631f621855b143a79f
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-17 14:11:55 -0300

    adicionando endpoint para add users

--- src/main/java/com/dojo/newprojectdojo/controllers/UserController.java
+++ src/main/java/com/dojo/newprojectdojo/controllers/UserController.java
@@ -4,10 +4,7 @@ import com.dojo.newprojectdojo.entities.User;
 import com.dojo.newprojectdojo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +24,12 @@ public class UserController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = userService.insert(obj);
         return ResponseEntity.ok().body(obj);
     }
 }
