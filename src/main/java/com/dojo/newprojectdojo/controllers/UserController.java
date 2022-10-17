bd9cc25a54b7d563de6ba0a4bb2661cf9a16b5f5
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-17 14:36:13 -0300

    corrigindo HttpCodes

--- src/main/java/com/dojo/newprojectdojo/controllers/UserController.java
+++ src/main/java/com/dojo/newprojectdojo/controllers/UserController.java
@@ -5,7 +5,9 @@ import com.dojo.newprojectdojo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,6 +32,7 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj = userService.insert(obj);
-        return ResponseEntity.ok().body(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
