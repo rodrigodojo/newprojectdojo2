9436eb33d1ff78d70032173a670b3a2443a429bf
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-17 16:21:38 -0300

    criando erros com HttpCodes personalizados parte 3

--- src/main/java/com/dojo/newprojectdojo/controllers/exceptions/StandardError.java
+++ src/main/java/com/dojo/newprojectdojo/controllers/exceptions/StandardError.java
@@ -1,5 +1,7 @@
 package com.dojo.newprojectdojo.controllers.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
 import java.time.Instant;
 
@@ -7,6 +9,7 @@ public class StandardError implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant timestamp;
     private Integer status;
     private String error;
--- src/main/java/com/dojo/newprojectdojo/services/UserService.java
+++ src/main/java/com/dojo/newprojectdojo/services/UserService.java
@@ -2,6 +2,7 @@ package com.dojo.newprojectdojo.services;
 
 import com.dojo.newprojectdojo.entities.User;
 import com.dojo.newprojectdojo.repositories.UserRepository;
+import com.dojo.newprojectdojo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
